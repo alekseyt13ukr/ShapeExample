ce398c4de9bc7c4c476d05605c78d02125d67fb0
alekseyt13ukr <alekseyt.ukr@mail.ru>
2015-08-14 20:05:34 +0300

    add junit tests

---
+++ src/LineTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by qa-mach3 on 14.08.15.
+ */
+public class LineTest {
+
+    @Test
+    public void testDraw() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEnd() throws Exception {
+
+    }
+
+    @Test
+    public void testSetEnd() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStart() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStart() throws Exception {
+
+    }
+}
---
+++ src/PointTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by qa-mach3 on 14.08.15.
+ */
+public class PointTest {
+
+    @Test
+    public void testRelativePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testGetX() throws Exception {
+
+    }
+
+    @Test
+    public void testSetX() throws Exception {
+
+    }
+
+    @Test
+    public void testGetY() throws Exception {
+
+    }
+
+    @Test
+    public void testSetY() throws Exception {
+
+    }
+}
